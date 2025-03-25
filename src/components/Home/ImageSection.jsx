8b814a7543a22a68afcb12eeaacf71f018b1380b
ANGELMUGIWARA <20233tn144@utez.edu.mx>
2025-03-25 10:24:39 -0600

    Imagen

--- src/components/Home/ImageSection.jsx
+++ src/components/Home/ImageSection.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import agentes from "./assets/image.png"; // Asegúrate de colocar la imagen en public o src/assets
+import agentes from "./assets/image.png"; // Asegúrate de que la ruta sea correcta
 
 const ImageSection = () => {
   return (
-    <div className="relative w-full lg:w-1/2 flex justify-center items-center h-screen">
-      {" "}
-      <img
-        src={agentes}
-        alt="Agentes"
-        className="absolute bottom-0 right-0 h-200 w-xl" 
-      />
+    <div className="relative w-full h-screen flex justify-center items-center overflow-hidden">
+      <div className="absolute inset-0 flex justify-center items-center">
+        <img
+          src={agentes}
+          alt="Agentes"
+          className="object-cover w-full h-full" // Ajusta la imagen para cubrir el contenedor
+        />
+      </div>
       <div className="absolute opacity-75 w-64 h-64 -z-10 transform rotate-45"></div>
     </div>
   );
